43628ad71b5a5ab8684a4ba05068f209deb69d58
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-04 17:59:34 -0500

    added clarifai face detection api

--- src/App.js
+++ src/App.js
@@ -1,68 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+  apiKey: 'fe15c2cf9cdd48c2a88a4514bba8eeb9'
+ });
 
 const particlesOptions = {
   "particles": {
     "number": {
-        "value": 160,
-        "density": {
-            "enable": false
-        }
+      "value": 160,
+      "density": {
+        "enable": false
+      }
     },
     "size": {
-        "value": 10,
-        "random": true
+      "value": 10,
+      "random": true
     },
     "move": {
-        "direction": "bottom",
-        "out_mode": "out"
+      "direction": "bottom",
+      "out_mode": "out"
     },
     "line_linked": {
-        "enable": false
+      "enable": false
     }
-},
-"interactivity": {
+  },
+  "interactivity": {
     "events": {
-        "onclick": {
-            "enable": true,
-            "mode": "remove"
-        }
+      "onclick": {
+        "enable": true,
+        "mode": "remove"
+      }
     },
     "modes": {
-        "remove": {
-            "particles_nb": 10
-        }
+      "remove": {
+        "particles_nb": 10
+      }
     }
-}
-  // particles: {
-  //   number: {
-  //     value: 30,
-  //     density: {
-  //       enable: true,
-  //       value_area: 800
-  //     }
-  //   }
-  // }
+  }
 
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
+    function(response) {
+      // do something with response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
   );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
