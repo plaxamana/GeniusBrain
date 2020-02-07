ac274332179904e248bb2ffdd4cd24943479ea26
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-06 20:42:21 -0500

    simple connection adding front end to back end request and responses

--- src/App.js
+++ src/App.js
@@ -61,7 +61,8 @@ class App extends Component {
       isSignedIn: false
     }
   }
-
+  
+  
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
